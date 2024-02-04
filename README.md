72b58d31019422e6578e1a90c15d4f2d800d0ac2
Alejandro Rodríguez <omar.alejandro.rodriguez@gmail.com>
2024-02-04 14:28:43 +0100

    Separate dependency injection example

--- README.md
+++ README.md
@@ -7,13 +7,21 @@ It requires cmocka installed. Ubuntu package: libcmocka-dev. Binaries available 
 ## Build the example
 The repository can be built with cmake. The generated binaries are:
 - src/app: an example dummy application
-- src/run_tests: example cmocka unit tests 
+- {build dir}/test/coin_tests: example cmocka unit tests 
+- {build dir}/test/coindi_tests: example cmocka unit tests using dependency injection instead of symbol replacement
+
+PRO TIP: define environment variable `CMOCKA_MESSAGE_OUTPUT=xml` when running the tests for JUnit output.
+```
+ CMOCKA_MESSAGE_OUTPUT=xml ./test/coin_tests
+```
 
 Alternatively, the app and tests can be built manually (e.g. with gcc)
 
 app:
 gcc src/myrandom.c src/main.c src/coin.c -I include/  -o app
 
+coin tests:
+gcc test/test_coin.c src/coin.c src/myrandom.c -lcmocka -I include/ -Wl,--wrap=myrandom_generate -o coin_tests
 tests:
-gcc test/test_coin.c src/coin.c src/myrandom.c -lcmocka -I include/ -Wl,--wrap=myrandom_generate -o run_tests
+gcc test/test_coindi.c src/coindi.c src/myrandom.c -lcmocka -I include/ -o coindi_tests
 
--- test/test_coin.c
+++ test/test_coin.c
@@ -12,16 +12,6 @@ int __wrap_myrandom_generate()
 {
     return mock_type(int);
 }
-
-int random_mock()
-{
-    return mock_type(int);
-}
-
-int random_stub()
-{
-    return 4;
-}
  
 static void test_coin_toss_returns_heads(void **state) 
 {
@@ -37,42 +27,11 @@ static void test_coin_toss_returns_tails(void **state)
     assert_int_equal(coin_toss(), COIN_TOSSRESULTS_TAILS);
 }
 
-static void test_coin_toss_with_DI_returns_heads(void **state) 
-{
-    (void) state; /* unused */
-    struct coin_descriptor cd;
-    cd.rand_func_ptr=random_mock;
-
-    will_return(random_mock, 1);
-    assert_int_equal(coin_di_toss(&cd), COINDI_TOSSRESULTS_HEADS);
-}
-
-static void test_coin_toss_with_DI_returns_tails(void **state) 
-{
-    (void) state; /* unused */
-    struct coin_descriptor cd;
-    cd.rand_func_ptr=random_mock;
-    
-    will_return(random_mock, 51);
-    assert_int_equal(coin_di_toss(&cd), COINDI_TOSSRESULTS_TAILS);
-}
-
-static void test_coin_toss_with_DI_and_stub_returns_heads(void **state) 
-{
-    (void) state; /* unused */
-    struct coin_descriptor cd;
-    cd.rand_func_ptr=random_stub;
-
-    assert_int_equal(coin_di_toss(&cd), COINDI_TOSSRESULTS_HEADS);
-}
- 
 int main(void) {
-    const struct CMUnitTest tests[] = {
+    const struct CMUnitTest tests[] = 
+    {
         cmocka_unit_test(test_coin_toss_returns_heads),
         cmocka_unit_test(test_coin_toss_returns_tails),
-        cmocka_unit_test(test_coin_toss_with_DI_returns_heads),
-        cmocka_unit_test(test_coin_toss_with_DI_returns_tails),
-        cmocka_unit_test(test_coin_toss_with_DI_and_stub_returns_heads),
     };
  
     return cmocka_run_group_tests(tests, NULL, NULL);
